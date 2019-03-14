ddb2309cbbab60525688e36197130d05c8412733
imJull <julljid@gmail.com>
2019-03-13 22:59:18 -0600

    Actualizado todo el video 6 y se arregló el guardar en la base de dats

--- app/src/main/java/unadeca/net/basedatos/activities/MainActivity.java
+++ app/src/main/java/unadeca/net/basedatos/activities/MainActivity.java
@@ -1,16 +1,23 @@
 package unadeca.net.basedatos.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.raizlabs.android.dbflow.sql.language.Delete;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 import java.util.List;
@@ -21,6 +28,7 @@ import unadeca.net.basedatos.database.models.Arbolito_Table;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CoordinatorLayout view;
     private ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +38,21 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         lista = findViewById(R.id.lista);
+        view = findViewById(R.id.content); //Coordinador que permite anclar vista
 
         ArrayAdapter<String> adaptador = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getArbolitos());
 
         lista.setAdapter(adaptador);
 
+        //Metodo para mostrar
         setAdapter();
 
-        Arbolito cedro = new Arbolito();
-        cedro.altura = 10;
-        cedro.fecha_plantado = "2017-01-01";
-        cedro.fecha_plantado = "2019-02-01";
-        cedro.plantado_por = "Martin Perez";
-        cedro.save();
+        //Arbolito cedro = new Arbolito();
+        //cedro.altura = 10;
+        //cedro.fecha_plantado = "2017-01-01";
+        //cedro.fecha_plantado = "2019-02-01";
+        //cedro.plantado_por = "Martin Perez";
+        //cedro.save();
 
 
 
@@ -63,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
                 //long contadoArbolitos = SQLite.selectCountOf().from(Arbolito.class).count();
                 //Snackbar.make(view, "Hay " + contadoArbolitos + " arbolitos registrados ", Snackbar.LENGTH_LONG)
                   //      .setAction("Action", null).show();
+
+                //EJEMPLO3 Implementacion dialogo
+                mostrarDialogo();
             }
         });
     }
@@ -100,5 +113,76 @@ public class MainActivity extends AppCompatActivity {
     //Adapdaor
     private void setAdapter() {
         lista.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getArbolitos()));
+    }
+
+    public void mostrarDialogo(){
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View formulario = layoutInflater.inflate(R.layout.formulari, null);
+
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(formulario);
+        final TextInputLayout altura = formulario.findViewById(R.id.txtAltura);
+        final TextInputLayout siembra = formulario.findViewById(R.id.txtFechaSiembra);
+        final TextInputLayout revision = formulario.findViewById(R.id.txtChequeo);
+        final TextInputLayout encargado = formulario.findViewById(R.id.txtEncargado);
+
+
+
+        builder.setMessage("LLene toda la información solicitada").setTitle("Crear nuevo arbolito").setCancelable(false)
+                .setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Try valida y el catch agarra los errores y muestra un mensaje.
+                try {
+                    validate(altura, siembra, revision, encargado);
+                    guardarABD(altura, siembra, revision, encargado);
+
+                }catch(Exception e){
+                    Snackbar.make(view, e.getMessage(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                }
+                dialog.dismiss();
+
+            }
+            //Boton Negtivo
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialogo = builder.create();
+        dialogo.show();
+    }
+
+    //Excepción por si hay espacios en blanco(Validación)
+    private void validate(TextInputLayout a, TextInputLayout s, TextInputLayout c, TextInputLayout e) throws Exception{
+        if(a.getEditText().getText().toString().isEmpty()) throw  new Exception("Se ocupa la altura del albolito");
+        if(s.getEditText().getText().toString().isEmpty()) throw  new Exception("Se ocupa la fecha de siembra del albolito");
+        if(a.getEditText().getText().toString().isEmpty()) throw  new Exception("Se ocupa la fecha del chequeo del albolito");
+        if(a.getEditText().getText().toString().isEmpty()) throw  new Exception("Se ocupa el nomre del encargado de albolito");
+    }
+
+    //Guardar en la base de datos
+    private void guardarABD(TextInputLayout a, TextInputLayout s, TextInputLayout c, TextInputLayout e){
+        Arbolito arbolito = new Arbolito();
+        arbolito.altura = Integer.parseInt(a.getEditText().getText().toString());
+        arbolito.fecha_plantado = s.getEditText().getText().toString();
+        arbolito.plantado_por = e.getEditText().getText().toString();
+        arbolito.fecha_ultima_revision = c.getEditText().getText().toString();
+        arbolito.save();
+        Snackbar.make(view, "Se ha guardado el arbolito", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+        setAdapter();
+
+    }
+
+    //Borrar algunos registros de las bases de datos
+    private void borrarArbolito(){
+        Delete.table(Arbolito.class);
+        SQLite.delete().from(Arbolito.class).where(Arbolito_Table.altura.between(1).and(1000)).execute();
     }
 }
